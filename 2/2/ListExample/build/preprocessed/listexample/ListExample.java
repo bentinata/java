cadf39eb28102157bf07c4ec8de7dbe2d6fc46f6
bentinata <bentinata@gmail.com>
2013-03-07 19:02:57 +0700

    new file:   2/2/ListExample/build.xml new file:   2/2/ListExample/build/.timestamp 	new file:   2/2/ListExample/build/compiled/listexample/ListExample.class 	new file:   2/2/ListExample/build/manifest.mf 	new file:   2/2/ListExample/build/preprocessed/listexample/ListExample.java 	new file:   2/2/ListExample/build/preverified/listexample/ListExample.class 	new file:   2/2/ListExample/build/preverifysrc/listexample/ListExample.class 	new file:   2/2/ListExample/dist/ListExample.jad 	new file:   2/2/ListExample/dist/ListExample.jar 	new file:   2/2/ListExample/nbproject/build-impl.xml 	new file:   2/2/ListExample/nbproject/genfiles.properties 	new file:   2/2/ListExample/nbproject/private/private.properties 	new file:   2/2/ListExample/nbproject/project.properties 	new file:   2/2/ListExample/nbproject/project.xml 	new file:   2/2/ListExample/src/listexample/ListExample.java

---
+++ 2/2/ListExample/build/preprocessed/listexample/ListExample.java
@@ -0,0 +1,58 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package listexample;
+
+import javax.microedition.lcdui.*;
+import javax.microedition.midlet.*;
+
+/**
+ @author bentinata
+ */
+public class ListExample extends MIDlet implements CommandListener {
+
+	Display display;
+	List list;
+	Command exitCommand = new Command("Exit", Command.EXIT, 1);
+	Command newCommand = new Command("New Item", Command.OK, 1);
+	Command renameCommand = new Command("Rename Item", Command.OK, 1);
+	Command deleteCommand = new Command("Delete Item", Command.OK, 1);
+	Ticker ticker = new Ticker(
+		"JENI- Java Education Network Indonesia");
+	public ListExample() {
+		list = new List("JENI : Contoh List", List.IMPLICIT);
+		list.append("List Item #1", null);
+		list.append("List Item #2", null);
+		list.append("List Item #3", null);
+		list.setTicker(ticker);
+		list.addCommand(exitCommand);
+		list.addCommand(newCommand);
+		list.addCommand(renameCommand);
+		list.addCommand(deleteCommand);
+		list.setCommandListener(this);
+	}
+	public void startApp() {
+		if (display == null) {
+			display = Display.getDisplay(this);
+			display.setCurrent(list);
+		}
+
+	}
+	public void pauseApp() {
+	}
+	public void destroyApp(boolean unconditional) {
+	}
+	public void commandAction(Command c, Displayable d) {
+		if (c == exitCommand) {
+			destroyApp(true);
+			notifyDestroyed(); // Exit
+		}
+		if (c == List.SELECT_COMMAND) {
+			int index = list.getSelectedIndex();
+			String currentItem = list.getString(index);
+// menjalankan suatu hal
+		}
+
+	}
+}
